2f6aec6480e683a38f9ed44f35b4b21009ffce17
YanyMolina <yaniza.molina@gmail.com>
2019-03-04 12:43:47 -0300

    Dom de categorias y nombre de usuario

--- src/data.js
+++ src/data.js
@@ -1,10 +1,33 @@
-/* Manejo de data */
+window.pokemonData = {
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+  //Funcion de orden.
+  sortData: (data, selectedOrder) => {
+ 
+ //Si el orden seleccionado es a-z, organiza y retorna la data con el nuevo orden
+    if (selectedOrder === "az") {
+      const sortedData = data;
+      data.sort(function (a, b) {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+      })
+      return sortedData;
+    }
+ 
+    //Si el orden seleccionado es z-a, organiza y retorna la data con el nuevo orden
+    if (selectedOrder === "za") {
+      const sortedData = data;
+      data.sort(function (a, b) {
+        if (b.name < a.name) {
+          return -1;
+        }
+        if (b.name > a.name) {
+          return 1;
+        }
+      })
+      return sortedData;
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -1,32 +1,34 @@
 /* Manejo del DOM */
 const btnCategorias = document.getElementsByTagName('button')[0];
 
-btnCategorias.addEventListener('click', (function Name() {
-  let nombre=document.getElementById("btnn").value;;
-  document.getElementById("nombre").innerHTML}) => {
+btnCategorias.addEventListener('click', () => {
+  let username= document.getElementById("name").value
   document.getElementById('root').innerHTML = '';
+  document.getElementById('category').innerHTML = '';
   document.getElementById('root').innerHTML += `<h1>Categorías</h1>
-  <h3>Bienvenido ${name}</h3>
-  <button class="water"><img src="img/Agua.png" alt=""</a></button>
-  <button class="fire"><img src="img/Fuego.png" alt=""></button>
-  <button class="grass"><img src="img/Planta.png" alt=""></button>
-  <button class="ground"><img src="img/Tierra.png" alt=""></button>
-  <button class="rock"><img src="img/Roca.png" alt=""></button>
-  <button class="ice"><img src="img/Hielo.png" alt=""></button>
-  <button class="electric"><img src="img/Electrico.png" alt=""></button>
-  <button class="dragon"><img src="img/Dragon.png" alt=""></button>
-  <button class="ghost"><img src="img/Fantasma.png" alt=""></button>
-  <button class="psychic"><img src="img/Psiquico.png" alt=""></button>
-  <button class="normal"><img src="img/Normal.png" alt=""></button>
-  <button class="fighting"><img src="img/Lucha.png" alt=""></button>
-  <button class="poison"><img src="img/Veneno.png" alt=""></button>
-  <button class="bug"><img src="img/Insecto.png" alt=""></button>
-  <button class="flying"><img src="img/Volador.png" alt=""></button>`
-})
+  <h3>Bienvenido ${username}</h3>
+  <button class="water"><img src="img/Agua.png" alt="Categoria Agua"></button>
+  <button class="fire"><img src="img/Fuego.png" alt="categoria Fuego"></button>
+  <button class="grass"><img src="img/Planta.png" alt="Categoria Planta"></button>
+  <button class="ground"><img src="img/Tierra.png" alt="Categoria Tierra"></button>
+  <button class="rock"><img src="img/Roca.png" alt="Categoria Roca"></button>
+  <button class="ice"><img src="img/Hielo.png" alt="Categoria Hielo"></button>
+  <button class="electric"><img src="img/Electrico.png" alt="Categoria Eléctrico"></button>
+  <button class="dragon"><img src="img/Dragon.png" alt="Categoria Dragón"></button>
+  <button class="ghost"><img src="img/Fantasma.png" alt="Categoria Fantasma"></button>
+  <button class="psychic"><img src="img/Psiquico.png" alt="Categoria Psiquico"></button>
+  <button class="normal"><img src="img/Normal.png" alt="Categoria Normal"></button>
+  <button class="fighting"><img src="img/Lucha.png" alt="Categoria Lucha"></button>
+  <button class="poison"><img src="img/Veneno.png" alt="Categoria Veneno"></button>
+  <button class="bug"><img src="img/Insecto.png" alt="Categoria Insecto"></button>
+  <button class="flying"><img src="img/Volador.png" alt="Categoria Volador"></button>`
+ })
 
 const btnAgua = document.getElementsByTagName('button')[1];
 
 btnAgua.addEventListener('click', () => {
+  document.getElementById('root').innerHTML = '';
   document.getElementById('category').innerHTML = '';
-  document.getElementById('category').innerHTML += `<a href="subcategorias.html"><h1>"Hola Yany"</h1>`
+  document.getElementById('category').innerHTML += `<p>Hola Yany</p>`
+
 })
