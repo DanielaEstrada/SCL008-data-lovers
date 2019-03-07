963a058971fe76c6122ee8ad263b1298fab6342a
YanyMolina <yaniza.molina@gmail.com>
2019-03-07 20:59:45 -0300

    corrigiendo errores y recuperando versión con filtro en la pagina de categoria

--- src/data.js
+++ src/data.js
@@ -3,13 +3,8 @@ function filterData(data, condition) {
  const filteredData = data.filter(element => {
       return element.type.includes(condition)
     })
-<<<<<<< HEAD
     return filteredData
   }
 
   window.filterData = filterData;
-  window.showData = showData;
-=======
-    return filteredData;
-};
->>>>>>> 74a30b523e86f4b6b38e7acccf24d4c74f79d280
+  
--- src/main.js
+++ src/main.js
@@ -23,12 +23,8 @@ const showData = (data) =>{
 data.forEach(element => {
   resultshowdata=pokeFilter.innerHTML ='';
   resultshowdata=pokeFilter.innerHTML +=
-<<<<<<< HEAD
-  `<div class="col s7 m4 l3">
-=======
   
-  `<div class="target col s7 m4 l3">
->>>>>>> 74a30b523e86f4b6b38e7acccf24d4c74f79d280
+  `<div class="col s7 m4 l3">
     <div class="card small">
     <a class="buttonCard" id="infobtn${element.id}" data-target="modal1">
     <div class="card-title">#${element.num} ${element.name}</div>
@@ -45,36 +41,29 @@ data.forEach(element => {
 return resultshowdata;
  
 };
-
 let select = document.getElementById("type");
 select.addEventListener("change", () => {
   let condition = select.value;
   //console.log(condition);
-  let resultFilter = window.filterData(data,condition);
+  let resultFilter = window.filterData( data,condition);
  pokeFilter.innerHTML="";//limpio el div
   resultFilter.forEach(element => {
 
   pokeFilter.innerHTML +=
-  `<div class="target col-xl-2 col-lg-3 col-md-6 col-sm6 col-8">
-    <div class="card">
-    <div class="img">
-    <div class="card-title">
-    <h2>${element.name}</h2>
+  `<div class="col s7 m4 l3">
+    <div class="card small">
+    <a class="buttonCard" id="infobtn${element.id}" data-target="modal1">
+    <div class="card-title">#${element.num} ${element.name}</div>
+    <div class="card-image" style="overflow: initial">
+    <img class="responsive-img" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${element.num}.png">
     </div>
-    <div class="image">
-    <img src=" ${element.img} ">
+    <div class="card-content">
+    <p>${element.type.join(" - ")}</p>
     </div>
-    <h4> #${element.num}</h4>
-    <h4>Tipo:${element.type.join(" - ")}</h4>
-    <p class="datos2">Posibilidad de encontrarlo: ${element.spawn_chance}%</p>
-    <p class="datos2">Hora de mayor actividad: ${element.spawn_time}hrs.</p>
-    <p class="datos2">Debilidad con Pókemon tipo: ${element.weaknesses}</p>  
-    </div>
+    </a>
     </div>
     </div>`
      });
 
 
 });
-
-window.onload = showData(data);
