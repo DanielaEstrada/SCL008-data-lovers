cea8a24111fc960f4b30162e96075c53e3c844df
“DanielaEstrada” <dani.strada@gmail.com>
2019-03-07 17:05:46 -0300

    mejoras de estilo en tarjetas

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ const showData = (data) =>{
 data.forEach(element => {
   resultshowdata=pokeFilter.innerHTML +=
   
-  `<div class="col s7 m4 l3">
+  `<div class="target col s7 m4 l3">
     <div class="card small">
     <a class="buttonCard" id="infobtn${element.id}" data-target="modal1">
     <div class="card-title">#${element.num} ${element.name}</div>
@@ -42,6 +42,7 @@ data.forEach(element => {
 return resultshowdata;
  
 };
+
 let select = document.getElementById("type");
 select.addEventListener("change", () => {
   let condition = select.value;
@@ -51,17 +52,18 @@ select.addEventListener("change", () => {
   resultFilter.forEach(element => {
 
   pokeFilter.innerHTML +=
-  `<div class="col s7 m4 l3">
-    <div class="card small">
-    <a class="buttonCard" id="infobtn${element.id}" data-target="modal1">
-    <div class="card-title">#${element.num} ${element.name}</div>
-    <div class="card-image" style="overflow: initial">
-    <img class="responsive-img" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${element.num}.png">
+  `<div class="target col-xl-2 col-lg-3 col-md-6 col-sm6 col-8">
+    <div class="card">
+    <div class="img">
+    <div class="card-title">
+    <h2>${element.name}</h2>
     </div>
-    <div class="card-content">
-    <p>${element.type.join(" - ")}</p>
+    <img src=" ${element.img} ">
     </div>
-    </a>
+    <h4> #${element.num}</h4>
+    <h4>Tipo:${element.type.join(" - ")}</h4>
+    <p>Debilidad con Pókemon tipo: ${element.weaknesses}</p>  
+    </div>
     </div>
     </div>`
      });
