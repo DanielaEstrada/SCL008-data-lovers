2d2c76c69f23066a022f2eea6c96d35926d40d5a
“DanielaEstrada” <dani.strada@gmail.com>
2019-03-07 14:32:39 -0300

    Función filter

--- src/data.js
+++ src/data.js
@@ -1,54 +1,7 @@
-window.pokemonData = {
 
-  //Funcion de orden.
-  sortData: (data, selectedOrder) => {
-
-    //Si el orden seleccionado es z-a, organiza y retorna la data con el nuevo orden
-    if (selectedOrder === "az") {
-      const sortedData = data;
-      data.sort(function (a, b) {
-        if (a.name < b.name) {
-          return -1;
-        }
-        if (a.name > b.name) {
-          return 1;
-        }
-      })
-      return sortedData;
-    }
-
-//Si el orden seleccionado es z-a, organiza y retorna la data con el nuevo orden
-    if (selectedOrder === "za") {
-      const sortedData = data;
-      data.sort(function (a, b) {
-        if (b.name < a.name) {
-          return -1;
-        }
-        if (b.name > a.name) {
-          return 1;
-        }
-      })
-      return sortedData;
-    }
-  },
-
-//Funcion de filtro.
-  filterData: (data, selectedType) => {
-
-    //Si el tipo seleccionado es el default, retorna la data sin ningun filtro
-    if (selectedType === "0") {
-      return data;
-    }
-
-    //Revisa todos los tipos de pokemon y retorna a la data solo los que coinciden con la seleccion
-    const filteredData = data.filter(element => {
-      return element.type[0] === selectedType || element.type[1] === selectedType;
+function filterData(data, condition) {
+ const filteredData = data.filter(element => {
+      return element.type.includes(condition)
     })
     return filteredData;
-  },
 
-  //Función de cálculo.
-  computeStats: (data) => {
-    return data.length;
-  }
-}
